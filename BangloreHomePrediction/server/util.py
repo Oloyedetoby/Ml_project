a9e8fbf7d7eed1b2172ddd594f52cce15325b926
Oloyedetoby <98145824+Oloyedetoby@users.noreply.github.com>
2022-03-17 12:19:13 +0100

    Add files via upload

---
+++ BangloreHomePrediction/server/util.py
@@ -0,0 +1,56 @@
+import json
+import pickle
+import numpy as np
+
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location,total_sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+
+    X = np.zeros(len(__data_columns))
+    X[0] = total_sqft
+    X[1] = bath
+    X[2] = bhk
+    if loc_index >= 0:
+        X[loc_index] = 1
+
+        return round(__model.predict([X])[0],2)
+
+
+def load_saved_artifacts():
+    print('loading saved artifacts.............')
+    global __data_columns
+    global __locations
+
+
+    with open('./artifacts/columns.json','r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]# first 3 columns are sqft, bath, bhk
+    
+
+    global __model
+    with open('./artifacts/housePrediction_model.pickle','rb') as f:
+        __model = pickle.load(f)
+        print('loading saved artifacts ............. done')        
+    return __locations
+    
+
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return __data_columns
+
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
